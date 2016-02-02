3311e4334977942fc4f29bf58c04030dc81975b2
Anton Rapetov <willir29@yandex.ru>
2016-02-02 19:29:19 +0300

    Convert boost::posix_time::ptime(NotADataTime) to json null

--- include/cereal/types/boost_posix_time.hpp
+++ include/cereal/types/boost_posix_time.hpp
@@ -31,17 +31,25 @@ inline void epilogue( JSONOutputArchive & ar, const boost::posix_time::ptime & )
 inline void CEREAL_SAVE_FUNCTION_NAME( JSONOutputArchive & ar, const boost::posix_time::ptime & time )
 {
   namespace pt = boost::posix_time;
-  pt::time_duration dur = time - pt::ptime(boost::gregorian::date(1970,1,1));
-  ar.saveValue(std::time_t(dur.ticks() / dur.ticks_per_second()));
+  if(!time.is_not_a_date_time()) {
+    pt::time_duration dur = time - pt::ptime(boost::gregorian::date(1970,1,1));
+    ar.saveValue(std::time_t(dur.ticks() / dur.ticks_per_second()));
+  } else {
+    ar.saveValue(nullptr);
+  }
 }
 
 inline void CEREAL_LOAD_FUNCTION_NAME( JSONInputArchive & ar, boost::posix_time::ptime & time )
 {
   namespace pt = boost::posix_time;
-  std::time_t tmp;
-  ar.loadValue(tmp);
-  pt::ptime start(boost::gregorian::date(1970,1,1));
-  time = start + pt::seconds(static_cast<int64_t>(tmp));
+  if(!ar.isNull()) {
+    std::time_t tmp;
+    ar.loadValue(tmp);
+    pt::ptime start(boost::gregorian::date(1970,1,1));
+    time = start + pt::seconds(static_cast<int64_t>(tmp));
+  } else {
+    time = pt::ptime{};  // Init to Not A Day Time
+  }
 }
 
 } // namespace cereall
